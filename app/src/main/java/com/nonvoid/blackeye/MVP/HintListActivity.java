31162b396140b9c8937b560b9df4686efffa76dd
mpgoodwin <mpgoodwi@go.olemiss.edu>
2016-06-21 21:24:52 -0500

    Getting started

---
+++ app/src/main/java/com/nonvoid/blackeye/MVP/HintListActivity.java
@@ -0,0 +1,27 @@
+package com.nonvoid.blackeye.MVP;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.nonvoid.blackeye.R;
+import com.nonvoid.blackeye.adapters.ListViewAdapter;
+import com.nonvoid.blackeye.io.InternalStorage;
+import com.nonvoid.blackeye.models.Hint;
+
+import java.util.ArrayList;
+
+public class HintListActivity extends AppCompatActivity {
+    ArrayList<Hint> hints;
+    ListView listView;
+    ListViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hint_list);
+        hints = InternalStorage.readHintList(this);
+        listView = (ListView) findViewById(R.id.listViewHints);
+        adapter = new ListViewAdapter(this, hints);
+    }
+}
---
+++ app/src/main/java/com/nonvoid/blackeye/adapters/ListViewAdapter.java
@@ -0,0 +1,37 @@
+package com.nonvoid.blackeye.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.nonvoid.blackeye.R;
+import com.nonvoid.blackeye.models.Hint;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Matt on 6/21/2016.
+ */
+public class ListViewAdapter extends ArrayAdapter<Hint> {
+    public ListViewAdapter(Context context, ArrayList<Hint> objects) {
+        super(context, 0, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Hint item = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_view_item, parent, false);
+        }
+        // Lookup view for data population
+        TextView title = (TextView) convertView.findViewById(R.id.textViewHintDescription);
+        title.setText(item.getDescription());
+
+        return convertView;
+    }
+}
