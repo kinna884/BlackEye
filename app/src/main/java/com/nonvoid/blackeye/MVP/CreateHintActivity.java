f1cdf182c2e9256111d3997db882e8bbf5dbfbd2
mpgoodwin <mpgoodwi@go.olemiss.edu>
2016-06-21 21:02:02 -0500

    Getting started

---
+++ app/src/main/java/com/nonvoid/blackeye/MVP/CreateHintActivity.java
@@ -0,0 +1,15 @@
+package com.nonvoid.blackeye.MVP;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nonvoid.blackeye.R;
+
+public class CreateHintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_hint);
+    }
+}
--- app/src/main/java/com/nonvoid/blackeye/MainActivity.java
+++ app/src/main/java/com/nonvoid/blackeye/MainActivity.java
@@ -1,7 +1,11 @@
 package com.nonvoid.blackeye;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.nonvoid.blackeye.MVP.CreateHintActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        //Testing
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.createHintButton :
+                Intent i = new Intent(this, CreateHintActivity.class);
+                startActivity(i);
+                break;
+
+            case  R.id.viewHintsButton :
+
+                break;
+        }
     }
 }
